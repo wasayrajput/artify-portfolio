b5d975dcc41aac2560ae33a47c1d2935eb7fbad4
wasayrajput <wasayrajput2450786123@gmail.com>
2026-01-22 00:04:56 +0500

    Added Instagram link, custom favicon, and site title

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer className="footer">
         <p>© {new Date().getFullYear} Artify Studio | Built with React.js</p>
         <div className="social-links">
-          <a href="https://www.instagram.com/aapka_username" target="_blank" rel="noreferrer">Instagram</a> • 
+          <a href="https://www.instagram.com/artify_studio_569?igsh=MW9pYXQ3bzdtaWd4Mg==" target="_blank" rel="noreferrer">Instagram</a> • 
           <a href="https://www.linkedin.com/in/abdul-wasay-dev" target="_blank" rel="noreferrer">LinkedIn</a> • 
           <a href="https://github.com/wasayrajput" target="_blank" rel="noreferrer">GitHub</a> •
           <a href="https://wa.me/923274760151" target="_blank" rel="noreferrer">Contact</a>
